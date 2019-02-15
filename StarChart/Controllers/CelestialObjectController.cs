05c93fb222ea1756946d6f0880f85b7d54c13d5f
Unknown <romain.avonde@capgemini.com>
2019-02-15 14:38:12 +0100

    Completed the module

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,30 @@
+﻿namespace StarChart.Controllers
+{
+    #region Usings
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using StarChart.Data;
+
+    #endregion
+
+    [ApiController]
+    [Route("")]
+    public class CelestialObjectController : ControllerBase
+    {
+        #region Champs
+
+        private readonly ApplicationDbContext _context;
+
+        #endregion
+
+        #region Constructeurs et destructeurs
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        #endregion
+    }
+}
